b846121903363784acbbf98609d508c393608dce
Баранова Екатерина Владимировна <e.baranova@sigma-it.ru>
2022-07-01 07:23:32 +0000

    Update QuestionOneAnswerTest.java

--- src/test/java/ru/baranova/spring/domain/QuestionOneAnswerTest.java
+++ src/test/java/ru/baranova/spring/domain/QuestionOneAnswerTest.java
@@ -3,17 +3,12 @@ package ru.baranova.spring.domain;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 
 @DisplayName("Test class QuestionOneAnswer")
 @TestPropertySource("classpath:questionOneAnswerTest.properties")
-@ContextConfiguration(classes = {QuestionOneAnswer.class})
 class QuestionOneAnswerTest {
-    @Autowired
-    QuestionOneAnswer questionOneAnswer;
     @Value("${test.bean.questionOneAnswer.question}")
     private String question;
     @Value("${test.bean.questionOneAnswer.rightAnswer}")
@@ -22,7 +17,7 @@ class QuestionOneAnswerTest {
     @Test
     void shouldGetNullOptionAnswers() {
         Question questionOneAnswer = new QuestionOneAnswer(question, new Answer(rightAnswer));
-        Assertions.assertEquals(questionOneAnswer.getOptionAnswers(), questionOneAnswer.getOptionAnswers());
+        Assertions.assertEquals(null, questionOneAnswer.getOptionAnswers());
     }
 
 }
