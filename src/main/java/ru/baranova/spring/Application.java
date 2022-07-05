e91380011fd1f90e0bc09f56163ce6711882a3df
Ekaterina Baranova <ebaranova@sigma-it.ru>
2022-07-05 11:52:51 +0300

    TR-61162: ДЗ4 log to file

--- src/main/java/ru/baranova/spring/Application.java
+++ src/main/java/ru/baranova/spring/Application.java
@@ -1,5 +1,6 @@
 package ru.baranova.spring;
 
+import ch.qos.logback.classic.util.ContextInitializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import ru.baranova.spring.services.TestServiceImpl;
@@ -7,6 +8,7 @@ import ru.baranova.spring.services.TestServiceImpl;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
+        //System.setProperty(ContextInitializer.CONFIG_FILE_PROPERTY, "logback-spring.xml");
         SpringApplication.run(Application.class, args)
                 .getBean(TestServiceImpl.class)
                 .test();
