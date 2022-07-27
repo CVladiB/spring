b9b8681f291dd34cbd6f1767e33fbc6ff17d6040
Ekaterina Baranova <ebaranova@sigma-it.ru>
2022-07-27 17:40:43 +0300

    TR-61162: hw7 text constants

--- src/main/java/ru/baranova/spring/domain/BusinessConstants.java
+++ src/main/java/ru/baranova/spring/domain/BusinessConstants.java
@@ -1,20 +1,28 @@
 package ru.baranova.spring.domain;
 
 public abstract class BusinessConstants {
-    public interface ShellEntityService {
+    public interface ShellEntityServiceLog {
         String COMPLETE_CREATE = "Новое поле добавлено";
         String COMPLETE_OUTPUT = "Корерктный вывод";
         String COMPLETE_UPDATE = "Поле изменено";
         String COMPLETE_DELETE = "Поле удалено";
+
         String WARNING = "Ошибка";
     }
-    public interface CheckConstant {
+
+    public interface CheckServiceLog {
         String NOTHING_INPUT = "Ничего не введено";
-        String SHOULD_EXIST_INPUT = "Нужнно ввести существующиее значение";
-        String WARNING_EXIST = "Ошибка, указанное поле уже существует";
+        String SHOULD_EXIST_INPUT = "Нужнно ввести существующее значение";
+        String WARNING_EXIST = "Указанное поле уже существует";
         String CHAR_OR_NUMBER_INPUT = "Недопустимый ввод, исключите символы";
         String SHORT_INPUT = "Слишком короткий ввод, минимум %d символов";
         String LONG_INPUT = "Слишком длинный ввод, максимум %d символов";
+    }
+
+    public interface LibraryServiceLog {
+        String WARNING_EXIST_MANY = "Существует несколько полей с указанными значениями, перепроверьте ввод или укажите Id";
+        String WARNING_CREATE = "Ошибка добавления поля, перепроверьте ввод или добавьте автора и жанры отдельно";
+
 
     }
 }
