398f00a504455452306cc52f205a2ad3b87d7456
Ekaterina Baranova <ebaranova@sigma-it.ru>
2022-07-28 16:15:28 +0300

    TR-61162: hw7 library shell controller

---
+++ src/main/java/ru/baranova/spring/controller/LibraryShellController.java
@@ -0,0 +1,102 @@
+package ru.baranova.spring.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+import ru.baranova.spring.domain.Book;
+import ru.baranova.spring.domain.BusinessConstants;
+import ru.baranova.spring.service.data.LibraryService;
+import ru.baranova.spring.service.print.visitor.EntityPrintVisitor;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@ShellComponent
+public class LibraryShellController {
+
+    private final LibraryService libraryServiceImpl;
+
+    private final EntityPrintVisitor printer;
+
+    @ShellMethod(group = "Book", value = "Create book", key = {"create-book"})
+    public String create(String title, String authorSurname, String authorName, String... genreArg) {
+        Book book = libraryServiceImpl.create(title, authorSurname, authorName, genreArg);
+        if (book != null) {
+            return BusinessConstants.ShellEntityServiceLog.COMPLETE_CREATE;
+        } else {
+            return BusinessConstants.ShellEntityServiceLog.WARNING;
+        }
+    }
+
+    @ShellMethod(group = "Book", value = "Create book", key = {"create-book-by-id"})
+    public String createById(String title, Integer authorId, Integer... genreArg) {
+        Book book = libraryServiceImpl.create(title, authorId, List.of(genreArg));
+        if (book != null) {
+            return String.format(BusinessConstants.ShellEntityServiceLog.COMPLETE_CREATE, book.getId());
+        } else {
+            return BusinessConstants.ShellEntityServiceLog.WARNING;
+        }
+    }
+
+    @ShellMethod(group = "Book", value = "Read book", key = {"read-book-id"})
+    public String readById(Integer id) {
+        Book book = libraryServiceImpl.readById(id);
+        if (book != null) {
+            printer.print(book);
+            return BusinessConstants.ShellEntityServiceLog.COMPLETE_OUTPUT;
+        } else {
+            return BusinessConstants.ShellEntityServiceLog.WARNING;
+        }
+    }
+
+    @ShellMethod(group = "Book", value = "Read book", key = {"read-book-title"})
+    public String readByTitle(String title) {
+        List<Book> bookList = libraryServiceImpl.readByTitle(title);
+        if (bookList != null) {
+            bookList.forEach(printer::print);
+            return BusinessConstants.ShellEntityServiceLog.COMPLETE_OUTPUT;
+        } else {
+            return BusinessConstants.ShellEntityServiceLog.WARNING;
+        }
+    }
+
+    @ShellMethod(group = "Book", value = "Read all book", key = {"read-all-books"})
+    public String readAll() {
+        List<Book> bookList = libraryServiceImpl.readAll();
+        if (!bookList.isEmpty()) {
+            bookList.forEach(printer::print);
+            return BusinessConstants.ShellEntityServiceLog.COMPLETE_OUTPUT;
+        } else {
+            return BusinessConstants.ShellEntityServiceLog.WARNING;
+        }
+    }
+
+    @ShellMethod(group = "Book", value = "Update book", key = {"update-book"})
+    public String update(Integer id, String title, String authorSurname, String authorName, String... genreArg) {
+        Book book = libraryServiceImpl.update(id, title, authorSurname, authorName, genreArg);
+        if (book != null) {
+            return BusinessConstants.ShellEntityServiceLog.COMPLETE_UPDATE;
+        } else {
+            return BusinessConstants.ShellEntityServiceLog.WARNING;
+        }
+    }
+
+    @ShellMethod(group = "Book", value = "Update book", key = {"update-book-by-id"})
+    public String updateById(Integer id, String title, Integer authorId, Integer... genreArg) {
+        Book book = libraryServiceImpl.update(id, title, authorId, List.of(genreArg));
+        if (book != null) {
+            return BusinessConstants.ShellEntityServiceLog.COMPLETE_UPDATE;
+        } else {
+            return BusinessConstants.ShellEntityServiceLog.WARNING;
+        }
+    }
+
+    @ShellMethod(group = "Book", value = "Delete book", key = {"delete-book"})
+    public String delete(Integer id) {
+        if (libraryServiceImpl.delete(id)) {
+            return BusinessConstants.ShellEntityServiceLog.COMPLETE_DELETE;
+        } else {
+            return BusinessConstants.ShellEntityServiceLog.WARNING;
+        }
+    }
+}
