e2d2a626e128f5f23a6b445f795573150584eae2
Ekaterina Baranova <ebaranova@sigma-it.ru>
2022-08-31 09:49:53 +0300

    TR-61162: hw16 genre (change shell controller to rest controller)

---
+++ src/main/java/ru/baranova/spring/controller/GenreRestController.java
@@ -0,0 +1,51 @@
+package ru.baranova.spring.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import ru.baranova.spring.model.Genre;
+import ru.baranova.spring.service.data.genre.GenreService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/genre")
+@RequiredArgsConstructor
+public class GenreRestController {
+    private final GenreService genreService;
+
+    @PutMapping("/gc")
+    public Genre create(@RequestParam("name") String name, @RequestParam("description") String description) {
+        return genreService.create(name, description);
+    }
+
+    @GetMapping("/gr-id")
+    public Genre readById(@RequestParam("id") Integer id) {
+        return genreService.readById(id);
+    }
+
+    @GetMapping("/gr")
+    public List<Genre> readByName(@RequestParam("name") String name) {
+        return genreService.readByName(name);
+    }
+
+    @GetMapping("/gr-all")
+    public List<Genre> readAll() {
+        return genreService.readAll();
+    }
+
+    @PatchMapping("/gu")
+    public Genre update(@RequestParam("id") Integer id, @RequestParam("name") String name, @RequestParam("description") String description) {
+        return genreService.update(id, name, description);
+    }
+
+    @DeleteMapping("/gd")
+    public void delete(@RequestParam("id") Integer id) {
+        genreService.delete(id);
+    }
+}
---
+++ src/test/java/ru/baranova/spring/controller/genre/GenreRestControllerTest.java
@@ -0,0 +1,103 @@
+package ru.baranova.spring.controller.genre;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import ru.baranova.spring.config.StopSearchConfig;
+import ru.baranova.spring.controller.GenreRestController;
+import ru.baranova.spring.model.Genre;
+import ru.baranova.spring.service.data.genre.GenreService;
+
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@WebMvcTest(value = {StopSearchConfig.class, GenreRestController.class})
+class GenreRestControllerTest {
+    @MockBean
+    private GenreService genreService;
+    @Autowired
+    private MockMvc mvc;
+    @Autowired
+    private ObjectMapper mapper;
+    private Genre genre;
+    private Genre insertGenre1;
+    private Genre insertGenre2;
+    private Genre testGenre;
+    private List<Genre> genreList;
+
+    @BeforeEach
+    void setUp() {
+        insertGenre1 = new Genre(1, "Name1", "Description1");
+        insertGenre2 = new Genre(2, "Name2", "Description2");
+        testGenre = new Genre(null, "NameTest", "DescriptionTest");
+        genreList = List.of(insertGenre1, insertGenre2);
+        genre = new Genre(7, "name", "description");
+    }
+
+    @Test
+    void create_correct() throws Exception {
+        Mockito.when(genreService.create(genre.getName(), genre.getDescription())).thenReturn(genre);
+        mvc.perform(put("/genre/gc")
+                        .param("name", genre.getName())
+                        .param("description", genre.getDescription()))
+                .andExpect(status().isOk())
+                .andExpect(content().json(mapper.writeValueAsString(genre)));
+    }
+
+    @Test
+    void readById_correct() throws Exception {
+        Mockito.when(genreService.readById(insertGenre1.getId())).thenReturn(insertGenre1);
+        mvc.perform(get("/genre/gr-id").param("id", insertGenre1.getId().toString()))
+                .andExpect(status().isOk())
+                .andExpect(content().json(mapper.writeValueAsString(insertGenre1)));
+    }
+
+    @Test
+    void readByName_correct() throws Exception {
+        Mockito.when(genreService.readByName(insertGenre1.getName())).thenReturn(List.of(insertGenre1));
+        mvc.perform(get("/genre/gr")
+                        .param("name", insertGenre1.getName()))
+                .andExpect(status().isOk())
+                .andExpect(content().json(mapper.writeValueAsString(List.of(insertGenre1))));
+    }
+
+
+    @Test
+    void readAll_correct() throws Exception {
+        Mockito.when(genreService.readAll()).thenReturn(genreList);
+        mvc.perform(get("/genre/gr-all"))
+                .andExpect(status().isOk())
+                .andExpect(content().json(mapper.writeValueAsString(genreList)));
+    }
+
+    @Test
+    void update_correct() throws Exception {
+        genre.setId(insertGenre1.getId());
+        Mockito.when(genreService.update(genre.getId(), genre.getName(), genre.getDescription())).thenReturn(genre);
+        mvc.perform(patch("/genre/gu")
+                        .param("id", genre.getId().toString())
+                        .param("name", genre.getName())
+                        .param("description", genre.getDescription()))
+                .andExpect(status().isOk())
+                .andExpect(content().json(mapper.writeValueAsString(genre)));
+    }
+
+    @Test
+    void delete_correct() throws Exception {
+        mvc.perform(delete("/genre/gd").param("id", insertGenre1.getId().toString()))
+                .andExpect(status().isOk());
+        Mockito.verify(genreService, Mockito.times(1)).delete(insertGenre1.getId());
+    }
+}
